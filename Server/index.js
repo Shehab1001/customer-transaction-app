28c7250100eeddbc984f131e3146cab1ec0f757d
Shehab Eldin Said <Shehab.eldin.said1@gmail.com>
2024-07-18 22:37:26 +0300

    handle server

--- Server/index.js
+++ Server/index.js
@@ -15,7 +15,7 @@ app.use('/api/data', dataRoutes);
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'Client', 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
