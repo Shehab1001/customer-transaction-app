91b7c31bd934ae13202033e506bfb903529d053f
Shehab Eldin Said <Shehab.eldin.said1@gmail.com>
2024-07-18 23:49:08 +0300

    fetch data

--- src/customerTable.jsx
+++ src/customerTable.jsx
@@ -20,7 +20,7 @@ const CustomerTable = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('data/data.json');
+        const response = await fetch('https://shehab1001.github.io/customer-transaction-app/data/data.json');
         const data = await response.json();
 
         setCustomers(data.customers);
